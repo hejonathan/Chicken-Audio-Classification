897e5e8b1252a8a5f57034fd6a47d6f27c200d2f
hejonathan <776177850he@gmail.com>
2022-06-10 20:56:23 +0000

    training

--- predict.py
+++ predict.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
                         help='fn to write predictions in logs dir')
     parser.add_argument('--src_dir', type=str, default='wavfiles',
                         help='directory containing wavfiles to predict')
-    parser.add_argument('--dt', type=float, default=1.0,
+    parser.add_argument('--dt', type=float, default=0.5,
                         help='time in seconds to sample audio')
     parser.add_argument('--sr', type=int, default=16000,
                         help='sample rate of clean audio')
